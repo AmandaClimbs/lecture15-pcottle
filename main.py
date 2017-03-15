1ccaa1606f011f54fc7a5c5ac475b8092bbcf882
Amanda <amanderson@csumb.edu>
2017-03-15 01:30:53 +0000

    Add meat

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+import random
 # Alright lets make a pizza!
 
 from __future__ import print_function
@@ -9,9 +9,13 @@ def getCheese():
 def getSauce():
     return 'some basic Marinara sauce'
 
+def getPepperoni():
+    return '%d pepperoni slices' % random.randrange(3, 10)
+    
 def getIngredients():
     return [
         getCheese(),
+        getPepperoni(),
         getSauce()
     ]
 
